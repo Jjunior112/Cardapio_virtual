a342470404e6d3fd95bd557bab5019bb42b7af6d
Jjunior112 <juarezgcjr@gmail.com>
2023-08-07 23:05:30 -0300

    hamburger menu e responsividade do carrinho

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,18 +1,42 @@
+import Hamburger from 'hamburger-react'
+import { useState } from 'react'
 import { SlBasket, SlBasketLoaded } from 'react-icons/sl'
 
 
-const NavBar = ({ msg, cart, isEmpty,showAll,showType1,showType2,showType3,showType4 }) => {
+const NavBar = ({ msg, cart, isEmpty, showAll, showType1, showType2, showType3, showType4 }) => {
 
-    
+    const [isOpen, setOpen] = useState(false);
+    const [showMenu, setShowMenu] = useState('hideMenu')
+    const hamburger = () => {
+        if (isOpen) {
+            setShowMenu('hideMenu');
+        }
+        else{
+            setShowMenu('hamburger-mobile')
+        }
+    }
+
 
     return (
         <nav>
             <div>
-            <button className='menu all' onClick={showAll}>Todos</button>
-                <button className='menu pizza' onClick={showType1}>Pizzas</button>
-                <button className='menu sandwich' onClick={showType2}>Sanduíches</button>
-                <button className='menu liquor' onClick={showType3}>Bebidas</button>
-                <button className='menu dessert' onClick={showType4}>Sobremesas</button>
+                <div className='hamburger-desktop'>
+                    <button className='menu all' onClick={showAll}>Todos</button>
+                    <button className='menu pizza' onClick={showType1}>Pizzas</button>
+                    <button className='menu sandwich' onClick={showType2}>Sanduíches</button>
+                    <button className='menu liquor' onClick={showType3}>Bebidas</button>
+                    <button className='menu dessert' onClick={showType4}>Sobremesas</button>
+                </div>
+                <Hamburger toggled={isOpen} toggle={setOpen} onToggle={hamburger} color='#910C00' />
+
+                <div className={showMenu}>
+
+                    <button className='menu all' onClick={showAll}>Todos</button>
+                    <button className='menu pizza' onClick={showType1}>Pizzas</button>
+                    <button className='menu sandwich' onClick={showType2}>Sanduíches</button>
+                    <button className='menu liquor' onClick={showType3}>Bebidas</button>
+                    <button className='menu dessert' onClick={showType4}>Sobremesas</button>
+                </div>
             </div>
 
             <div className='cartNav'>
