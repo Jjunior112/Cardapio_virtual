4befa5f1aa14c9360b2fd465c250851693e196ce
Jjunior112 <juarezgcjr@gmail.com>
2023-08-28 19:40:30 -0300

    refactor

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,6 +1,9 @@
 import Hamburger from 'hamburger-react'
 import { useState } from 'react'
 import { SlBasket, SlBasketLoaded } from 'react-icons/sl'
+import { FaPizzaSlice } from 'react-icons/fa'
+import { GiHamburger, GiCakeSlice } from 'react-icons/gi'
+import { MdLiquor } from 'react-icons/md'
 
 
 const NavBar = ({ msg, cart, isEmpty, showAll, showType1, showType2, showType3, showType4 }) => {
@@ -11,7 +14,7 @@ const NavBar = ({ msg, cart, isEmpty, showAll, showType1, showType2, showType3, 
         if (isOpen) {
             setShowMenu('hideMenu');
         }
-        else{
+        else {
             setShowMenu('hamburger-mobile')
         }
     }
@@ -22,22 +25,22 @@ const NavBar = ({ msg, cart, isEmpty, showAll, showType1, showType2, showType3, 
             <div>
                 <div className='hamburger-desktop'>
                     <button className='menu all' onClick={showAll}>Todos</button>
-                    <button className='menu pizza' onClick={showType1}>Pizzas</button>
-                    <button className='menu sandwich' onClick={showType2}>Sanduíches</button>
-                    <button className='menu liquor' onClick={showType3}>Bebidas</button>
-                    <button className='menu dessert' onClick={showType4}>Sobremesas</button>
+                    <button className='menu pizza' onClick={showType1}><FaPizzaSlice /></button>
+                    <button className='menu sandwich' onClick={showType2}><GiHamburger /></button>
+                    <button className='menu liquor' onClick={showType3}><MdLiquor /></button>
+                    <button className='menu dessert' onClick={showType4}><GiCakeSlice /></button>
                 </div>
                 <div className='hamburger' >
-                <Hamburger toggled={isOpen} toggle={setOpen} onToggle={hamburger} color='#910C00' aria-label='botao para filtrar os itens por categoria'  />
+                    <Hamburger toggled={isOpen} toggle={setOpen} onToggle={hamburger} color='#910C00' aria-label='botao para filtrar os itens por categoria' />
                 </div>
 
                 <div className={showMenu}>
 
                     <button className='menu all' onClick={showAll}>Todos</button>
-                    <button className='menu pizza' onClick={showType1}>Pizzas</button>
-                    <button className='menu sandwich' onClick={showType2}>Sanduíches</button>
-                    <button className='menu liquor' onClick={showType3}>Bebidas</button>
-                    <button className='menu dessert' onClick={showType4}>Sobremesas</button>
+                    <button className='menu pizza' onClick={showType1}><FaPizzaSlice /></button>
+                    <button className='menu sandwich' onClick={showType2}><GiHamburger /></button>
+                    <button className='menu liquor' onClick={showType3}><MdLiquor /></button>
+                    <button className='menu dessert' onClick={showType4}><GiCakeSlice /></button>
                 </div>
             </div>
 
